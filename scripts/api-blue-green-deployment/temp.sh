606f30aea94d50108a3093c4196820182a955747
shvedas <shvedas@gmail.com>
2018-05-21 09:22:28 +0300

    test build

--- scripts/api-blue-green-deployment/temp.sh
+++ scripts/api-blue-green-deployment/temp.sh
@@ -1,10 +1,10 @@
 deployapi
 dev-api
 shvedas
-git access token: 1c1f492cef788f57157c504f2c991c19244ad926
+git access token: 9a165a991913f47ad99e025ac2d0530d72abf6b7
 
 #!/bin/bash
 
-export AWS_ACCESS_KEY_ID=AKIAJWYPYZZ3HL7K6D3A
-export AWS_SECRET_ACCESS_KEY=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
+export AWS_ACCESS_KEY_ID=AKIAIMXEYHZO3WFJRBLA
+export AWS_SECRET_ACCESS_KEY=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
 # export AWS_DEFAULT_REGION=us-east-1
